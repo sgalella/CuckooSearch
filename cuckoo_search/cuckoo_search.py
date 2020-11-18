46e252ad696f55df96a7d62633bcfc5da27755fa
sgalella <santi.galella@gmail.com>
2020-11-18 22:11:07 +0100

    Add docstrings

--- cuckoo_search/cuckoo_search.py
+++ cuckoo_search/cuckoo_search.py
@@ -4,7 +4,17 @@ from scipy.stats import levy
 
 
 class CuckooSearch:
+    """ Implements the Cuckoo Search algorithm proposed in Cuckoo Search via Lévy Flights. """
     def __init__(self, num_individuals, landscape, alpha=1, c=1.5):
+        """
+        Initialization of the main parameters of the algorithm.
+
+        Args:
+            num_individuals (int): Number of nests.
+            landscape (FitnessLandscape): Fitness landscape to evaluate the cuckoo search.
+            alpha (int, optional): Parameter to scale the step size. Defaults to 1.
+            c (float, optional): Controls the tail of the lévy distribution. Set between 1 and 3. Defaults to 1.5.
+        """
         self.alpha = alpha
         self.num_individuals = num_individuals
         self.landscape = landscape
@@ -13,12 +23,27 @@ class CuckooSearch:
         self.levy = levy(scale=c)
 
     def _initialize_population(self):
+        """
+        Initializes each nest in a random position in the landscape.
+
+        Returns:
+            np.array: Coordinates for each nest.
+        """
         population = np.random.random((self.num_individuals, 2))
         population[:, 0] = (self.landscape.limits[1] - self.landscape.limits[0]) * population[:, 0] + self.landscape.limits[0]
         population[:, 1] = (self.landscape.limits[3] - self.landscape.limits[2]) * population[:, 1] + self.landscape.limits[2]
         return population
 
     def _levy_flight(self, ind1):
+        """
+        Computes the levy flight for an individual.
+
+        Args:
+            ind1 (int): Index of the selected individual to move.
+
+        Returns:
+            tuple: Updated position after the lévy flight.
+        """
         angle = 2 * np.pi * np.random.random()
         move = self.alpha * np.multiply(self.levy.rvs(), [np.cos(angle), np.sin(angle)])
         position = self.population[ind1, :] + move
@@ -30,6 +55,7 @@ class CuckooSearch:
         return position
 
     def update(self):
+        """ Runs one iteration of the algorithm. """
         ind1, ind2 = np.random.choice(self.num_individuals, size=(2, ), replace=False)
         position = self._levy_flight(ind1)
         fitness_ind1 = self.landscape.evaluate_fitness(position)
@@ -40,6 +66,13 @@ class CuckooSearch:
 
 
 def visualize_all(algorithm, num_iterations):
+    """
+     Shows the evolution of the solutions in the landscape.
+
+    Args:
+        algorithm (CuckooSearch): Cuckoo search initialized.
+        num_iterations (int): Number of iterations to run the algorithm.
+    """
     plt.figure(figsize=(8, 5))
     algorithm.landscape.plot()
     plt.colorbar(shrink=0.75)
@@ -57,6 +90,13 @@ def visualize_all(algorithm, num_iterations):
 
 
 def visualize_end(algorithm, num_iterations):
+    """
+    Shows the last evolution of the solutions in the landscape.
+
+    Args:
+        algorithm (CuckooSearch): Cuckoo search initialized.
+        num_iterations (int): Number of iterations to run the algorithm.
+    """
     for _ in range(num_iterations):
         algorithm.update()
     plt.figure(figsize=(8, 5))
@@ -68,6 +108,13 @@ def visualize_end(algorithm, num_iterations):
 
 
 def no_visualization(algorithm, num_iterations):
+    """
+    Runs the algorithm without visualization. Prints the best solution obtained.
+
+    Args:
+        algorithm (CuckooSearch): Cuckoo search initialized.
+        num_iterations (int): Number of iterations to run the algorithm.
+    """
     for _ in range(num_iterations):
         algorithm.update()
     print(algorithm.best_fitness)
--- cuckoo_search/optimization_functions.py
+++ cuckoo_search/optimization_functions.py
@@ -6,7 +6,15 @@ from scipy import spatial
 
 
 class FitnessLandscape(ABC):
+    """ Template for building landscapes. """
     def __init__(self, limits, resolution):
+        """
+        Initialize bounds and fitness function.
+
+        Args:
+            limits (list): Boundaries of the landscape: [x_min, x_max, y_min, y_max]
+            resolution (int): Number of points per dimension.
+        """
         self.limits = limits
         self.resolution = resolution
         self.X, self.Y = self._create_meshgrid()
@@ -15,26 +23,52 @@ class FitnessLandscape(ABC):
         self.max, self.min = np.max(self.fitness_function), np.min(self.fitness_function)
 
     def _generate_coords(self):
+        """
+        Generates array of coordinates and tree for positions interpolation.
+
+        Returns:
+            tuple: Coordinates and tree for position lookup.
+        """
         coords = np.dstack([self.X.ravel(), self.Y.ravel()])[0]
         return coords, spatial.cKDTree(coords)
 
     def _create_meshgrid(self):
+        """
+        Builds up the grid of the landscape. Each point corresponds to a coordinate in the space.
+
+        Returns:
+            tuple: Arrays containing coordinates of the meshgrid.
+        """
         x = np.linspace(self.limits[0], self.limits[1], self.resolution)
         y = np.linspace(self.limits[2], self.limits[3], self.resolution)
         X, Y = np.meshgrid(x, y)
         return X, Y
 
     def evaluate_fitness(self, pos):
+        """
+        Computes the fitness of individual at position given the fitness function of the landscape.
+
+        Args:
+            pos (tuple): x and y of the individual
+
+        Returns:
+            float: Normalized fitness of the individual. Individuals in the minima will have a fitness close to 1.
+        """
         _, index = self.tree.query(pos)
         return 1 - (self.fitness_function[index // self.resolution][index % self.resolution] - self.min) / (self.max - self.min)
 
     def plot(self):
+        """ Displays the landscape using contour maps. """
         cs = plt.contour(self.X, self.Y, self.fitness_function)
         plt.clabel(cs, inline=1, fontsize=6)
         plt.imshow(self.fitness_function, extent=self.limits, origin="lower", alpha=0.3)
 
     @abstractmethod
     def _calculate_fitness(self):
+        """ Creates the fitness landscape given a function.
+            Check https://en.wikipedia.org/wiki/Test_functions_for_optimization?wprov=srpw1_0
+            for more information.
+        """
         pass
 
 
